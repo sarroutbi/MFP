e805f815b948d890604f117da993756c81c6993a
sarroutbi <sarroutbi@yahoo.es>
2014-05-16 14:15:09 +0200

    Adding new final report structure

---
+++ opendaylight/img/Scenario for Localization.mm
@@ -0,0 +1,73 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#669900" CREATED="1339004269563" ID="ID_126273993" MODIFIED="1339005131773">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="5">Scenario for Localization</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#006633" CREATED="1339004269563" HGAP="-109" ID="ID_481622242" MODIFIED="1339005150085" POSITION="left" VSHIFT="49">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="5">Proprietary Software</font>
+    </p>
+    <ul>
+      <li>
+        License: Developer controls
+      </li>
+      <li>
+        Developer provides translations
+      </li>
+      <li>
+        Users depend on developer for new translations
+      </li>
+      <li>
+        Developer hires translation services
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<cloud/>
+</node>
+<node COLOR="#006633" CREATED="1339004269563" HGAP="-116" ID="ID_1402973492" MODIFIED="1339005142894" POSITION="right" VSHIFT="96">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="5">Libre Software</font>
+    </p>
+    <ul>
+      <li>
+        License: Users control
+      </li>
+      <li>
+        Both developer and users provide translation
+      </li>
+      <li>
+        Both developer and users may hire translation services
+      </li>
+      <li>
+        Everybody benefits from new translations
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<cloud/>
+</node>
+</node>
+</map>
